dab2633c89465848c0251604ed36ae7330923c5f
Gonzalo Pereira <pereiradossantosgonzalo@gmail.com>
2022-10-10 18:51:33 -0300

    Services component added

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,7 +4,24 @@ import './Services.css';
 const Services = () => {
   return (
     <section id='services'>
-      
+      <h5>What do I offer?</h5>
+      <h2>Services</h2>
+      <div className="container services_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+            <ul className="service_list">
+              <li>Responsive web design</li>
+              <li>RESTful API's</li>
+              <li>UX/UI design</li>
+              <li>Interactive SPA's</li>
+              <li>Relational and not relational database development</li>
+              <li>Clean and maintainable code for your web</li>
+            </ul>
+          
+        </article>
+      </div>
     </section>
   )
 }
